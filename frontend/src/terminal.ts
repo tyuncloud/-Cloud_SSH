baffc526c2e60cafe536595db5e5c6033f2bcaa1
tyuncloud <xcyunidc@gmail.com>
2026-08-11 18:37:44 +0800

    Update terminal.ts

--- frontend/src/terminal.ts
+++ frontend/src/terminal.ts
@@ -306,7 +306,7 @@ document.getElementById('connection-port');
 
     cursorStyle: 'block',
 
-    fontSize: 10,
+     fontSize: window.innerWidth <= 768 ? 6 : 13,
 
     fontFamily: '"JetBrains Mono", "Fira Code", "Cascadia Code", monospace',
 
